69c362148ebf904370b96f53f92f3a0fa7b84a17
Yauheniya Strelkova <Yauheniya_Strelkova@epam.com>
2020-05-09 19:17:15 +0300

    Added OnSerializing method for class Category

---
+++ Task/DB/CategorySerializable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.DB
+{
+    [Serializable]
+    public partial class Category
+    {
+        [OnSerializing]
+        public void OnSerializing(StreamingContext context)
+        {
+            if (context.Context is SerializationObject serializationObject && serializationObject.Type == typeof(Category))
+            {
+                serializationObject.ObjectContext.LoadProperty(this, c => c.Products);
+            }
+        }
+    }
+}
